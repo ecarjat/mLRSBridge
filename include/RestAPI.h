21935ece676d224a9ff37993ffe3f16bd2d043b6
Emmanuel Carjat <emmanuel.carjat@quanthouse.com>
2024-07-23 23:55:14 +0200

    Added web page

--- include/RestAPI.h
+++ include/RestAPI.h
@@ -4,10 +4,60 @@
 #include <ESPAsyncWebServer.h>
 #include <ArduinoJson.h>
 #include <AsyncJson.h>
+#include <SPIFFS.h>
 #include "txMLRS.h"
 #include "logger.h"
 
 static const char *REST_TAG = "RESTAPI";
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">
+</head>
+<body>
+  <p><h1>File Upload</h1></p>
+  <p>Free Storage: %FREESPIFFS% | Used Storage: %USEDSPIFFS% | Total Storage: %TOTALSPIFFS%</p>
+  <form method="POST" action="/upload" enctype="multipart/form-data"><input type="file" name="data"/><input type="submit" name="upload" value="Upload" title="Upload File"></form>
+  <p>After clicking upload it will take some time for the file to firstly upload and then be written to SPIFFS, there is no indicator that the upload began.  Please be patient.</p>
+  <p>Once uploaded the page will refresh and the newly uploaded file will appear in the file list.</p>
+  <p>If a file does not appear, it will be because the file was too big, or had unusual characters in the file name (like spaces).</p>
+  <p>You can see the progress of the upload by watching the serial output.</p>
+</body>
+</html>
+)rawliteral";
+
+static const char upload_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">
+</head>
+<body>
+  <p><h1>Firmware uploaded, ready to flash</h1></p>
+  <p>Free Storage: %FREESPIFFS% | Used Storage: %USEDSPIFFS% | Total Storage: %TOTALSPIFFS%</p>
+  <form method="POST" action="/flash"><input type="submit" name="Flash" value="Flash" title="Flash"></form>
+</body>
+</html>
+)rawliteral";
+
+static const char flash_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">
+</head>
+<body>
+  <p><h1>Firmware flashed</h1></p>
+  <p>Free Storage: %FREESPIFFS% | Used Storage: %USEDSPIFFS% | Total Storage: %TOTALSPIFFS%</p>
+</body>
+</html>
+)rawliteral";
+
 
 #define JSONSIZE 2048
 
@@ -22,9 +72,11 @@ class RestAPI : public AsyncWebServer
     void add_json_object(const char *tag, float value, const char *unit);
     void add_json_object(const char *tag, String value, const char *unit);
     void handleUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final);
+    void flashFirmware();
 
 public:
     RestAPI(txMLRS *txMLRS, int port = 80);
-   
+    static String humanReadableSize(const size_t bytes);
+    static String processor(const String &var);
 };
 #endif
--- src/RestAPI.cpp
+++ src/RestAPI.cpp
@@ -1,5 +1,97 @@
 #include "RestAPI.h"
 
+RestAPI::RestAPI(txMLRS *txMLRS, int port) : _txMLRS(txMLRS), AsyncWebServer(port)
+{
+    if (!SPIFFS.begin(true))
+    {
+        // if you have not used SPIFFS before on a ESP32, it will show this error.
+        // after a reboot SPIFFS will be configured and will happily work.
+        logE(REST_TAG, "%s", "ERROR: Cannot mount SPIFFS, Rebooting");
+        ESP.restart();
+    }
+    // JSON handler
+    AsyncCallbackJsonWebHandler *handler = new AsyncCallbackJsonWebHandler(
+        "/command",
+        [this](AsyncWebServerRequest *request, JsonVariant &json)
+        { this->jsonCommand(request, json); });
+    addHandler(handler);
+
+    // landing page
+    on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+       {
+    logD(REST_TAG, "Client: %s %s", request->client()->remoteIP().toString(), request->url());
+    request->send(200, "text/html", index_html, RestAPI::processor); });
+
+    // after firmware upload
+    on("/ready", HTTP_GET, [](AsyncWebServerRequest *request)
+       {
+    logD(REST_TAG, "Client: %s %s", request->client()->remoteIP().toString(), request->url());
+    request->send(200, "text/html", upload_html, RestAPI::processor); });
+
+    // Flashing firmware
+    on("/flash", HTTP_POST, [this](AsyncWebServerRequest *request)
+       {
+    logD(REST_TAG, "Client: %s %s", request->client()->remoteIP().toString(), request->url());
+    this->flashFirmware();
+    request->send(200, "text/html", flash_html, RestAPI::processor); });
+
+    // process upload file
+    on("/upload", HTTP_POST, [](AsyncWebServerRequest *request)
+       { request->send(200); }, [this](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final)
+       { this->handleUpload(request, filename, index, data, len, final); });
+
+    DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
+    onNotFound([](AsyncWebServerRequest *request)
+               {
+        if (request->method() == HTTP_OPTIONS) {
+            request->send(200);
+        } else {
+            request->send(404);
+    } });
+    begin();
+}
+
+void RestAPI::flashFirmware(){
+    logD(REST_TAG, "%s", "flashing firmware");
+    File flash_file = SPIFFS.open("/firmware.bin", "rb");
+    _txMLRS->flash(&flash_file);
+}
+
+/**
+ * @brief 
+ * source: https://github.com/smford/esp32-asyncwebserver-fileupload-example/blob/master/example-01/example-01.ino
+ */
+void RestAPI::handleUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final)
+{
+    String logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url();
+    Serial.println(logmessage);
+
+    if (!index)
+    {
+        logmessage = "Upload Start: " + String(filename);
+        // open the file on first call and store the file handle in the request object
+        request->_tempFile = SPIFFS.open("/firmware.bin", "w");
+        Serial.println(logmessage);
+    }
+
+    if (len)
+    {
+        // stream the incoming chunk to the opened file
+        request->_tempFile.write(data, len);
+        logmessage = "Writing file: " + String(filename) + " index=" + String(index) + " len=" + String(len);
+        Serial.println(logmessage);
+    }
+
+    if (final)
+    {
+        logmessage = "Upload Complete: " + String(filename) + ",size: " + String(index + len);
+        // close the file handle as the upload is now done
+        request->_tempFile.close();
+        Serial.println(logmessage);
+        request->redirect("/ready");
+    }
+}
+
 void RestAPI::jsonCommand(AsyncWebServerRequest *request, JsonVariant &json)
 {
     if (not json.is<JsonObject>())
@@ -11,11 +103,12 @@ void RestAPI::jsonCommand(AsyncWebServerRequest *request, JsonVariant &json)
 
     if (not data["command"].is<String>())
     {
-       request->send(400, "application/json", "{\"response\":\"no command\"}");
+        request->send(400, "application/json", "{\"response\":\"no command\"}");
         return;
     }
     String commandLine = data["command"].as<String>();
-    if (commandLine.isEmpty()){
+    if (commandLine.isEmpty())
+    {
         request->send(400, "application/json", "{\"response\":\"no command\"}");
         return;
     }
@@ -34,25 +127,6 @@ void RestAPI::jsonCommand(AsyncWebServerRequest *request, JsonVariant &json)
         }
         request->send(200, "application/json", "{\"response\":\"" + _txMLRS->response() + "\"}");
     }
-}
-
-RestAPI::RestAPI(txMLRS *txMLRS, int port) : _txMLRS(txMLRS), AsyncWebServer(port)
-{
-    AsyncCallbackJsonWebHandler *handler = new AsyncCallbackJsonWebHandler(
-        "/command",
-        [this](AsyncWebServerRequest *request, JsonVariant &json)
-        { this->jsonCommand(request, json); });
-    addHandler(handler);
-
-    DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
-    onNotFound([](AsyncWebServerRequest *request)
-               {
-        if (request->method() == HTTP_OPTIONS) {
-            request->send(200);
-        } else {
-            request->send(404);
-    } });
-    begin();
 }
 
 void RestAPI::create_json(char *tag, float value, char *unit)
@@ -78,4 +152,42 @@ void RestAPI::add_json_object(const char *tag, String value, const char *unit)
     obj["type"] = tag;
     obj["value"] = value;
     obj["unit"] = unit;
+}
+
+// Make size of files human readable
+// source: https://github.com/CelliesProjects/minimalUploadAuthESP32
+String RestAPI::humanReadableSize(const size_t bytes)
+{
+    if (bytes < 1024)
+        return String(bytes) + " B";
+    else if (bytes < (1024 * 1024))
+        return String(bytes / 1024.0) + " KB";
+    else if (bytes < (1024 * 1024 * 1024))
+        return String(bytes / 1024.0 / 1024.0) + " MB";
+    else
+        return String(bytes / 1024.0 / 1024.0 / 1024.0) + " GB";
+}
+
+/**
+ * @brief 
+ * source: https://github.com/smford/esp32-asyncwebserver-fileupload-example/blob/master/example-01/example-01.ino
+ */
+String RestAPI::processor(const String &var)
+{
+    if (var == "FREESPIFFS")
+    {
+        return RestAPI::humanReadableSize((SPIFFS.totalBytes() - SPIFFS.usedBytes()));
+    }
+
+    if (var == "USEDSPIFFS")
+    {
+        return RestAPI::humanReadableSize(SPIFFS.usedBytes());
+    }
+
+    if (var == "TOTALSPIFFS")
+    {
+        return RestAPI::humanReadableSize(SPIFFS.totalBytes());
+    }
+
+    return String();
 }
